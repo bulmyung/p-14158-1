bedfe0a52bcc95d7c08938ed1439924c9ffbf229
bulmyung <bulmyun9@gmail.com>
2026-06-02 17:30:08 +0900

    setting

---
+++ src/main/java/com/back/P141581Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P141581Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P141581Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/P141581ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P141581ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
