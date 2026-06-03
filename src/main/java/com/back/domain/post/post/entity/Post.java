db114498a6ffc01c3188e8a1330d8cbc865c89f7
bulmyung <bulmyun9@gmail.com>
2026-06-03 15:40:45 +0900

    22

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -11,19 +11,23 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private int id;
+    private final int id;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.id = 0;
         this.title = title;
         this.content = content;
+    }
+
+    public Post() {
+        this(null, null);
     }
 }
 
