31899014877f6d6d99b69b2cc99ca8b9794ab350
bulmyung <bulmyun9@gmail.com>
2026-06-03 21:04:26 +0900

    56

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.entity;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -10,13 +11,14 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 public class Post extends BaseEntity {
-    private int authorId;
+    @ManyToOne
+    private Member author;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    public Post(int authorId, String title, String content) {
-        this.authorId = authorId;
+    public Post(Member author, String title, String content) {
+        this.author = author;
         this.title = title;
         this.content = content;
     }
