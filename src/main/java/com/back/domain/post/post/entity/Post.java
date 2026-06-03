abe4cc48c0642db19b7c532588a4ff2156e5aeb6
bulmyung <bulmyun9@gmail.com>
2026-06-03 18:12:26 +0900

    40

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,36 +1,20 @@
 package com.back.domain.post.post.entity;
 
+import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private int id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Post extends BaseEntity {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
