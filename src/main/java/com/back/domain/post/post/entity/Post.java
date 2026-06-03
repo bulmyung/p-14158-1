7f78dd87098a0adc8f3c6b25c34048ec4a470d9e
bulmyung <bulmyun9@gmail.com>
2026-06-03 15:13:43 +0900

    17

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,6 +17,7 @@ public class Post {
     @GeneratedValue(strategy = IDENTITY)
     private int id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String content;
 }
 
